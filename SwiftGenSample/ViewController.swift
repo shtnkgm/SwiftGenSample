beedcbdbe4448af97a724bddeb4af9df17d96ca0
shtnkgm <nakagami.shota@gmail.com>
2018-11-20 00:48:58 +0900

    fix

--- SwiftGenSample/ViewController.swift
+++ SwiftGenSample/ViewController.swift
@@ -12,9 +12,18 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let numberImages = Asset.allImages.filter { $0.name.hasNamespacePath(of: Asset.Number.self) }
+        numberImages.forEach { print($0.name) }
+        
+        let animalImages = Asset.allImages.filter { $0.name.hasNamespacePath(of: Asset.Animal.self) }
+        animalImages.forEach { print($0.name) }
     }
-
-
 }
 
+extension String {
+    func hasNamespacePath(of assetType: Any) -> Bool {
+        guard let namespace = self.split(separator: "/").first else { return false }
+        return namespace.capitalized == String(describing: assetType.self)
+    }
+}
